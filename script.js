94d843e4a62655c7970ea1451b7c0a917fcabf9d
felipewantstocode <118938786+felipewantstocode@users.noreply.github.com>
2023-01-15 17:59:35 -0300

    Implement proper display for each round, modify display functions

--- script.js
+++ script.js
@@ -11,62 +11,91 @@ function firstUpperRestLower(string) {
 function play(userChoice, computerChoice) {
     switch (userChoice) {
         case computerChoice:
-            return "draw";
+            return "Draw";
         case "Rock": 
             if (computerChoice === "Paper") {
-                return "computer";
+                return "Computer";
             }
             if (computerChoice === "Scissors") {
-                return "player";
+                return "Player";
             }
         case "Paper":
             if (computerChoice === "Scissors") {
-                return "computer";
+                return "Computer";
             }
             if (computerChoice === "Rock") {
-                return "player";
+                return "Player";
             }
         case "Scissors":
             if (computerChoice === "Rock") {
-                return "computer";
+                return "Computer";
             }
             if (computerChoice === "Paper") {
-                return "player";
+                return "Player";
             }
     }
 }
 
-function gameOver() {
+function isGameOver() {
     if (playerScore >= 5 || computerScore >= 5) return true;
 }
 
-function printResult (winner, playerChoice, computerChoice) {
-    const thisRound = document.querySelector('#this-round');
-    const pScore = document.querySelector('#player-score');
-    const cScore = document.querySelector('#computer-score');
-    thisRound.textContent = "This round winner: " + winner;
-    pScore.textContent = "Player: " + playerScore + " points";
-    cScore.textContent = "Computer: " + computerScore + " points";
+function gameOver() {
+    let champ = ""; 
+    playerScore >= 5 ? champ = "Player" : champ = "Computer";
+    const body = document.querySelector('body');
+    const finalScore = document.createElement('div');
+    const restart = document.createElement('button');
+    finalScore.classList.add('final');
+    restart.classList.add('final');
+    restart.textContent = 'Play again';
+    restart.onclick = () => freshStart(); 
+    finalScore.textContent = `Game finished. ${champ} wins.` 
+    body.appendChild(finalScore);
+    body.appendChild(restart);
+}
 
-    if (gameOver()) {
-        let champ = "";
-        playerScore >= 5 ? champ = "Player" : champ = "Computer";
-        const body = document.querySelector('body');
-        const finalScore = document.createElement('div');
-        const restart = document.createElement('button');
-        finalScore.classList.add('final');
-        restart.classList.add('final');
-        restart.textContent = 'Play again';
-        restart.onclick = () => freshStart(); 
-        finalScore.textContent = `Game finished. ${champ} wins.` 
-        body.appendChild(finalScore);
-        body.appendChild(restart);
+function printCurrentPlay (winner, playerChoice, computerChoice) {
+    if (gameStarted) {
+    const arenaDisplay = document.querySelector("#arena");
+    arenaDisplay.innerHTML = `Player chose ${playerChoice}<br>Computer chose ${computerChoice}`;
+    const thisRound = document.querySelector('#this-round');
+    if (winner === "Draw") {
+        thisRound.textContent = "Draw";
+    } else {
+        switch (winner) {
+            case "Player": thisRound.textContent = `${playerChoice} beats ${computerChoice}`;
+            case "Computer": thisRound.textContent = `${computerChoice} beats ${playerChoice}`;
+        }
+    }
+    printScore();
     }
 }
+
+function printScore () {
+    if (gameStarted) {
+        const pScore = document.querySelector('#player-score');
+        const cScore = document.querySelector('#computer-score');
+        pScore.textContent = "Player: " + playerScore + " points";
+        cScore.textContent = "Computer: " + computerScore + " points";
+        if (isGameOver()) gameOver();
+    }
+}
+
+
 function freshStart () {
     playerScore = 0;
     computerScore = 0;
-    printResult("");
+    gameStarted = false;
+    const arenaDisplay = document.querySelector("#arena");
+    arenaDisplay.textContent = "";
+    const thisRound = document.querySelector('#this-round');
+    thisRound.textContent = "";
+
+    const pScore = document.querySelector('#player-score');
+    const cScore = document.querySelector('#computer-score');
+    pScore.textContent = "";
+    cScore.textContent = "";
 
     const final = document.querySelectorAll('.final');
     if(final) {
@@ -77,6 +106,7 @@ function freshStart () {
     }
 }
 
+let gameStarted = false;
 let playerScore = 0;
 let computerScore = 0; 
 freshStart();
@@ -84,38 +114,45 @@ freshStart();
 
 const btnRock = document.querySelector('#rock');
 btnRock.addEventListener('click', () => {
-    if (gameOver()) return;
-    const thisRound = play ('Rock', getComputerChoice());
-    if (thisRound === 'computer') {
+    gameStarted = true;
+    if (isGameOver()) return;
+    const computerChoice = getComputerChoice();
+    const thisRound = play ('Rock', computerChoice);
+    if (thisRound === 'Computer') {
         computerScore += 1;
     }
-    if (thisRound ==="player") { 
+    if (thisRound ==="Player") { 
         playerScore += 1 };
-    printResult(thisRound);
+    // console.log(thisRound);
+    printCurrentPlay(thisRound, "Rock", computerChoice);
     });
 
 
 const btnPaper = document.querySelector('#paper');
 btnPaper.addEventListener('click', () => {
-    if (gameOver()) return;
-    const thisRound = play ('Paper', getComputerChoice());
-    if (thisRound === 'computer') {
+    gameStarted = true;
+    if (isGameOver()) return;
+    const computerChoice = getComputerChoice();
+    const thisRound = play ('Paper', computerChoice);
+    if (thisRound === 'Computer') {
         computerScore += 1;
     }
-    if (thisRound ==="player") { 
+    if (thisRound ==="Player") { 
         playerScore += 1 };
-    printResult(thisRound);
+    printCurrentPlay(thisRound, "Paper", computerChoice);
     });
 
 const btnScissors = document.querySelector('#scissors');
 btnScissors.addEventListener('click', () => {
-    if (gameOver()) return;
-    const thisRound = play ('Scissors', getComputerChoice());
-    if (thisRound === 'computer') {
+    gameStarted = true;
+    if (isGameOver()) return;
+    const computerChoice = getComputerChoice();
+    const thisRound = play ('Scissors', computerChoice);
+    if (thisRound === 'Computer') {
         computerScore += 1;
     }
-    if (thisRound ==="player") { 
+    if (thisRound ==="Player") { 
         playerScore += 1 };
-    printResult(thisRound);
+    printCurrentPlay(thisRound, "Scissors", computerChoice);
     });
 
